c751acd6230f6622acdf6c41aca0761cc67d1b83
iamrivindu <rivindu.15@cse.mrt.ac.lk>
2017-07-12 23:24:42 +0530

    Create complain.php

---
+++ complain.php
@@ -0,0 +1,30 @@
+<html>
+<h2> Create Complain</h2>
+<body>
+<form  action="">
+<input type="hidden" name="action" value="submit">
+Your name:<br>
+<input name="name" type="text" value="" size="30"/><br>
+Your email:<br>
+<input name="email" type="text" value="" size="30"/><br>
+Your complain:<br>
+<textarea name="message" rows="7" cols="30"></textarea><br>
+    
+Position :<br>
+<select name= "position" id="position"><br>
+	
+<option value="0"> Select your position </option>
+<option value="maintain"> Maintainance worker </option>
+<option value="technical"> Technical engineer </option>
+<option value="chief"> Chief engineer </option><br>
+	
+<br><input type="submit" value="Send email"/>
+	
+</form>
+</body>
+</html>
+
+<?php
+   
+?>
+						
